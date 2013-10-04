70dac955cfb674b47956eca83f11926f796f82e0
DsTroyed <teddy.freddy@live.com>
2013-10-04 21:17:35 +0200

    updated Battlefield.java

--- battlefield 3/src/me/sirantony/battlefield/Battlefield.java
+++ battlefield 3/src/me/sirantony/battlefield/Battlefield.java
@@ -1,5 +1,26 @@
 package me.sirantony.battlefield;
 
-public class Battlefield {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Battlefield extends JavaPlugin{
+	
+	public Battlefield pl;
+	
+	public void onEnable(){
+		//load configs
+		
+		//load modules
+		
+		//load games
+		
+		//load weapons...
+		pl = this;
+	}
+	public void onDisable(){
+		pl = null;
+	}
+	
+	public Battlefield getInstance(){
+		return pl;
+	}
 }
